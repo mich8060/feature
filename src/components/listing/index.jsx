de58d3619ad46366a94b9b226e3b84851defc58b
Michael Stevens <mich8060@hotmail.com>
2023-11-12 23:45:15 -0700

    Removing dependancy on their api.

--- src/components/listing/index.jsx
+++ src/components/listing/index.jsx
@@ -1,33 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './listing.scss';
-import axios from 'axios';
 
 import Facet from '../facet';
 import NumberFormat from '../numberFormat';
 import Product from '../product';
 import Toolbar from '../toolbar';
 
-const productAPI = 'https://api.fastsimon.com/categories_navigation?request_source=v-next&src=v-next&UUID=64e915b4-459d-4dbd-97e9-dc5225e455de&uuid=64e915b4-459d-4dbd-97e9-dc5225e455de&store_id=4089909&api_type=json&category_id=26051221&facets_required=1&products_per_page=60&page_num=2&with_product_attributes=true&sort_by=popular';
+import productAPI from './listing.json';
 
 export default function Listing(){
 
     const scrollableElementRef = useRef(null);
-    
-    const [facets, setFacets] = useState([]);
-    const [result, setResult] = useState([]);
-    const [product, setProduct] = useState([]);
     const [initialScrollPosition, setInitialScrollPosition] = useState(0);
-    const total_results = <NumberFormat amount={result} style="number" />;
-
-    axios.get(productAPI)
-    .then((response) => {
-        setResult(response.data.total_results);
-        setProduct(response.data.items);
-        setFacets(response.data.facets);
-    })
-    .catch((error) => {
-        console.error('Error fetching data:', error);
-    });
+    const total_results = <NumberFormat amount={productAPI.total_results} style="number" />;
 
     useEffect(() => {
         const calculateInitialScrollPosition = () => {
@@ -56,9 +41,9 @@ export default function Listing(){
             <div className="listing--body">
                 <div className="listing--facets">
                     <div className="listing--overflow" ref={scrollableElementRef}>
-                        {facets.length > 0 ? (
+                        {productAPI.facets.length > 0 ? (
                             <>
-                                {facets.map((result, index) => (
+                                {productAPI.facets.map((result, index) => (
                                     <Facet 
                                         data={result[1]}
                                         handleScroll={handleScroll}
@@ -74,9 +59,9 @@ export default function Listing(){
                     </div>
                 </div>
                 <div className="listing--products">
-                    {product.length > 0 ? (
+                    {productAPI.items.length > 0 ? (
                         <>
-                            {product.map((result, index) => (
+                            {productAPI.items.map((result, index) => (
                                 <Product 
                                     href={result.u}
                                     brand={result.v}
